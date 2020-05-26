e172885fda8b933b9a51180cc42e66b79c11e393
Anthony Starks <ajstarks@gmail.com>
2020-05-26 11:34:58 -0400

    rename cmd to play

--- play/main.go
+++ play/main.go
@@ -20,14 +20,21 @@ func fade(c *giocanvas.Canvas, y, width, size, interval float32, fint uint8, col
 	}
 }
 
+func rn(n int) float32 {
+	return float32(rand.Intn(n))
+}
+
 // randrect randomly  places random sized rectangles
-func randrect(c *giocanvas.Canvas, w, h float32, n int) {
+func randrect(c *giocanvas.Canvas, n int) {
 	color := color.RGBA{R: 0, G: 0, B: 0, A: 255}
 	for i := 0; i < n; i++ {
-		x := rand.Float32() * w
-		y := rand.Float32() * h
-		w := rand.Float32() * 100
-		h := rand.Float32() * 100
+		x := rn(100)
+		y := rn(100)
+		w := rn(10)
+		h := rn(10)
+		if y < 50 || x > 50 {
+			continue
+		}
 		color.R = uint8(rand.Intn(255))
 		color.G = uint8(rand.Intn(255))
 		color.B = uint8(rand.Intn(255))
@@ -46,7 +53,6 @@ func main() {
 	size := app.Size(unit.Dp(width), unit.Dp(height))
 	title := app.Title("Gio Canvas")
 	tcolor := color.RGBA{128, 0, 0, 255}
-	gcolor := color.RGBA{0, 0, 0, 128}
 	fcolor := color.RGBA{0, 0, 128, 255}
 
 	go func() {
@@ -55,18 +61,16 @@ func main() {
 		for e := range w.Events() {
 			if e, ok := e.(system.FrameEvent); ok {
 				canvas.Context.Reset(e.Queue, e.Config, e.Size)
-				canvas.Text(width*0.60, height*0.55, 40, "Follow your dreams", tcolor)
-				canvas.Text(width*0.10, height*0.55, 40, "Random", tcolor)
-				canvas.CenterImage("follow.jpg", width*0.75, height*0.25, 816, 612, 75)
-				randrect(canvas, width/2, height/2, 300)
-				canvas.Grid(width, height, 2, 20, gcolor)
-				fade(canvas, height*0.75, width, 20, 20, 10, fcolor)
-
-				for x := float32(0); x <= canvas.Width; x += canvas.Width / 20 {
+				canvas.Text(60, 45, 3, "Follow your dreams", tcolor)
+				canvas.Text(10, 45, 3, "Random", tcolor)
+				canvas.CenterImage("follow.jpg", 75, 75, 816, 612, 75)
+				fade(canvas, 25, 100, 2.0, 20, 10, fcolor)
+				randrect(canvas, 200)
+				for x := float32(0); x <= 100; x += 10 {
 					v := fmt.Sprintf("%v", x)
-					canvas.Text(x, height*0.80, 15, v, tcolor)
-					canvas.TextMid(x, height*0.85, 15, v, tcolor)
-					canvas.TextEnd(x, height*0.90, 15, v, tcolor)
+					canvas.Text(x, 20, 1.5, v, tcolor)
+					canvas.TextMid(x, 15, 1.5, v, tcolor)
+					canvas.TextEnd(x, 10, 1.5, v, tcolor)
 				}
 				e.Frame(canvas.Context.Ops)
 			}
