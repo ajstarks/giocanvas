3e29aee6fab923ea15fe504116f28eb62d83b9e8
Anthony Starks <ajstarks@gmail.com>
2020-05-29 14:55:28 -0400

    add polygon and lines

--- giocanvas.go
+++ giocanvas.go
@@ -13,6 +13,7 @@ import (
 	"gioui.org/font/gofont"
 	"gioui.org/layout"
 	"gioui.org/op"
+	"gioui.org/op/clip"
 	"gioui.org/op/paint"
 	"gioui.org/text"
 	"gioui.org/unit"
@@ -37,13 +38,36 @@ func NewCanvas(width, height float32) *Canvas {
 	return canvas
 }
 
+// pct returns the percentage of its input
 func pct(p float32, m float32) float32 {
 	return ((p / 100.0) * m)
 }
 
-// dimen returns canvas dimensions from percentages (converting from x increasing left-right, y increasing top-bottom)
+// dimen returns canvas dimensions from percentages
+// (converting from x increasing left-right, y increasing top-bottom)
 func dimen(xp, yp, w, h float32) (float32, float32) {
 	return pct(xp, w), pct(100-yp, h)
+}
+
+// Line makes a stroked line from (x0, y0) to (x1, y1) using percentage-based measures
+func (c *Canvas) Line(x0, y0, x1, y1, size float32, color color.RGBA) {
+	x0, y0 = dimen(x0, y0, c.Width, c.Height)
+	x1, y1 = dimen(x1, y1, c.Width, c.Height)
+	size = pct(size, c.Width)
+	c.AbsLine(x0, y0, x1, y1, size, color)
+}
+
+// Polygon makes a filled polygon with vertices in x and y, using percentage-based measures
+func (c *Canvas) Polygon(x, y []float32, color color.RGBA) {
+	if len(x) != len(y) || len(x) < 3 {
+		return
+	}
+	nx := make([]float32, len(x))
+	ny := make([]float32, len(y))
+	for i := 0; i < len(x); i++ {
+		nx[i], ny[i] = dimen(x[i], y[i], c.Width, c.Height)
+	}
+	c.AbsPolygon(nx, ny, color)
 }
 
 // Text places text using percentage-based measures
@@ -217,4 +241,81 @@ func (c *Canvas) AbsCenterImage(name string, x, y float32, w, h int, scale float
 	ops := c.Context.Ops
 	paint.NewImageOp(im).Add(ops)
 	paint.PaintOp{Rect: f32.Rect(x, y, x+imw, y+imh)}.Add(ops)
+}
+
+// AbsPolygon makes a closed, filled polygon with vertices in x and y
+func (c *Canvas) AbsPolygon(x, y []float32, color color.RGBA) {
+	if len(x) != len(y) {
+		return
+	}
+	path := new(clip.Path)
+	ops := c.Context.Ops
+	r := f32.Rect(0, 0, c.Width, c.Height)
+	var stack op.StackOp
+	defer stack.Pop()
+	stack.Push(c.Context.Ops)
+
+	path.Begin(ops)
+	path.Move(f32.Point{X: x[0], Y: y[0]})
+
+	l := len(x)
+	point := f32.Point{}
+	for i := 1; i < l; i++ {
+		path.Line(f32.Point{X: x[i] - x[i-1], Y: y[i] - y[i-1]})
+	}
+	path.Line(f32.Point{X: x[0] - x[l-1], Y: y[0] - y[l-1]})
+	path.Line(point)
+	path.End().Add(ops)
+	paint.ColorOp{Color: color}.Add(ops)
+	paint.PaintOp{Rect: r}.Add(ops)
+}
+
+// quadline makes a four-sided polygon with vertices at (p0, p1, p2, p3) forming a line
+func (c *Canvas) quadline(p0, p1, p2, p3 f32.Point, color color.RGBA) {
+	path := new(clip.Path)
+	ops := c.Context.Ops
+	r := f32.Rect(0, 0, c.Width, c.Height)
+	var stack op.StackOp
+	defer stack.Pop()
+	stack.Push(c.Context.Ops)
+	path.Begin(ops)
+	path.Move(p0)
+	path.Line(f32.Point{X: p1.X - p0.X, Y: p1.Y - p0.Y})
+	path.Line(f32.Point{X: p2.X - p1.X, Y: p2.Y - p1.Y})
+	path.Line(f32.Point{X: p3.X - p2.X, Y: p3.Y - p2.Y})
+	path.Line(f32.Point{X: p0.X - p3.X, Y: p0.Y - p3.Y})
+	path.End().Add(ops)
+	paint.ColorOp{Color: color}.Add(ops)
+	paint.PaintOp{Rect: r}.Add(ops)
+}
+
+// AbsLine makes a line from (x0,y0) to (x1, y1) using absolute coordinates
+func (c *Canvas) AbsLine(x0, y0, x1, y1, lw float32, color color.RGBA) {
+	l2 := lw / 2
+
+	// vertical line
+	if x0 == x1 {
+		c.AbsRect(x0, y0, lw, y1-y0, color)
+		return
+	}
+	// horizontal line
+	if y0 == y1 {
+		c.AbsRect(x0, y0-l2, x1-x0, lw, color)
+		return
+	}
+	// init for positive sloped line
+	p0 := f32.Point{X: x0 - l2, Y: y0 - l2}
+	p1 := f32.Point{X: x0 + l2, Y: y0 + l2}
+	p2 := f32.Point{X: x1 + l2, Y: y1 + l2}
+	p3 := f32.Point{X: x1 - l2, Y: y1 - l2}
+
+	// adjust for negative slope
+	if y0 < y1 {
+		p0.Y = y0 + l2
+		p1.Y = y0 - l2
+		p2.Y = y1 - l2
+		p3.Y = y1 + l2
+	}
+
+	c.quadline(p0, p1, p2, p3, color)
 }
