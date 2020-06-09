95638762fabe611203b6ef00357eff91115d1a32
Anthony Starks <ajstarks@gmail.com>
2020-06-09 15:05:25 -0400

    update conentric client

--- concentric/main.go
+++ concentric/main.go
@@ -8,22 +8,21 @@ import (
 	"gioui.org/app"
 	"gioui.org/io/system"
 	"gioui.org/unit"
-	"github.com/ajstarks/giocanvas"
+	gc "github.com/ajstarks/giocanvas"
 )
 
 func concentric(s string, w, h int) {
 	width, height := float32(w), float32(h)
 	win := app.NewWindow(app.Title(s), app.Size(unit.Px(width), unit.Px(height)))
-	r := float32(30)
-	g := uint8(5)
+
 	for e := range win.Events() {
 		if e, ok := e.(system.FrameEvent); ok {
-			canvas := giocanvas.NewCanvas(width, height, e.Config, e.Queue, e.Size)
-			canvas.CenterRect(50, 50, 100, 100, color.RGBA{0, 0, 0, 255})
-			for i := 0; i < 6; i++ {
-				canvas.Circle(50, 50, r, color.RGBA{128, g, g, 255})
-				r -= 5
-				g += 25
+			canvas := gc.NewCanvas(width, height, e.Config, e.Queue, e.Size)
+			canvas.Background(gc.ColorLookup("white"))
+			var r float32 = 50
+			for g := uint8(0); g < 250; g += 50 {
+				canvas.Circle(50, 50, r, color.RGBA{g, g, g, 255})
+				r -= 10
 			}
 			e.Frame(canvas.Context.Ops)
 		}
