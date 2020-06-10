01469ae11b1b1ba6b5fef61b71c0bd5d526427e8
Anthony Starks <ajstarks@gmail.com>
2020-06-10 11:41:21 -0400

    update twrap to show API

--- twrap/main.go
+++ twrap/main.go
@@ -20,6 +20,7 @@ func work(title string, w, h int) {
 	win := app.NewWindow(app.Title(title), app.Size(unit.Px(width), unit.Px(height)))
 	var ts float32 = 3
 	var lw float32 = 0.1
+	subsize := ts * 0.6
 	gray := color.RGBA{128, 128, 128, 255}
 	red := color.RGBA{128, 0, 0, 255}
 	green := color.RGBA{0, 128, 0, 255}
@@ -29,6 +30,9 @@ func work(title string, w, h int) {
 			canvas := giocanvas.NewCanvas(width, height, e)
 			s := "Now is the time for all good men to come to the aid of the party, and the quick brown fox jumped over the lazy dog."
 			var left float32 = 20
+			canvas.Text(left, 90, subsize, "TextWrap(x, y, size, s, 60, red)", red)
+			canvas.Text(left, 70, subsize, "TextWrap(x, y, size, s, 40, red)", green)
+			canvas.Text(left, 40, subsize, "TextWrap(x, y, size, s, 20, red)", blue)
 			canvas.Line(left, 0, left, 100, lw*2, gray)
 			canvas.Line(left+60, 85+ts, left+60, 0, lw, red)
 			canvas.Line(left+40, 65+ts, left+40, 0, lw, green)
