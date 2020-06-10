757dd880d1cb336a5a32a5debac8ca7d102eefb4
Anthony Starks <ajstarks@gmail.com>
2020-06-10 14:15:23 -0400

    use shading to denote wrap region

--- twrap/main.go
+++ twrap/main.go
@@ -18,28 +18,38 @@ func pct(p float32, m float32) float32 {
 func work(title string, w, h int) {
 	width, height := float32(w), float32(h)
 	win := app.NewWindow(app.Title(title), app.Size(unit.Px(width), unit.Px(height)))
-	var ts float32 = 3
-	var lw float32 = 0.1
+	var ts float32 = 2.8
 	subsize := ts * 0.6
-	gray := color.RGBA{128, 128, 128, 255}
+	gray := color.RGBA{100, 100, 100, 255}
 	red := color.RGBA{128, 0, 0, 255}
 	green := color.RGBA{0, 128, 0, 255}
 	blue := color.RGBA{0, 0, 128, 255}
 	for e := range win.Events() {
 		if e, ok := e.(system.FrameEvent); ok {
 			canvas := giocanvas.NewCanvas(width, height, e)
-			s := "Now is the time for all good men to come to the aid of the party, and the quick brown fox jumped over the lazy dog."
-			var left float32 = 20
-			canvas.Text(left, 90, subsize, "TextWrap(x, y, size, s, 60, red)", red)
-			canvas.Text(left, 70, subsize, "TextWrap(x, y, size, s, 40, red)", green)
-			canvas.Text(left, 40, subsize, "TextWrap(x, y, size, s, 20, red)", blue)
-			canvas.Line(left, 0, left, 100, lw*2, gray)
-			canvas.Line(left+60, 85+ts, left+60, 0, lw, red)
-			canvas.Line(left+40, 65+ts, left+40, 0, lw, green)
-			canvas.Line(left+20, 35+ts, left+20, 0, lw, blue)
-			canvas.TextWrap(left, 85, ts, 60, s, red)
-			canvas.TextWrap(left, 65, ts, 40, s, green)
-			canvas.TextWrap(left, 35, ts, 20, s, blue)
+			s := "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+			var left float32 = 15
+			var y1, y2, y3, ys float32
+
+			y1 = 95
+			y2 = y1 - 20
+			y3 = y2 - 25
+			ys = ts * 2
+
+			canvas.Text(left, y1, subsize, "TextWrap(x, y, size, s, 60, red)", gray)
+			canvas.Text(left, y2, subsize, "TextWrap(x, y, size, s, 40, red)", gray)
+			canvas.Text(left, y3, subsize, "TextWrap(x, y, size, s, 20, red)", gray)
+			//canvas.Line(left, 0, left, 100, lw*2, gray)
+
+			red.A, green.A, blue.A = 50, 50, 50
+			canvas.CornerRect(left, y1-ts, 60, ts*4, red)
+			canvas.CornerRect(left, y2-ts, 40, ts*6, green)
+			canvas.CornerRect(left, y3-ts, 20, ts*13, blue)
+			red.A, green.A, blue.A = 255, 255, 255
+
+			canvas.TextWrap(left, y1-ys, ts, 60, s, red)
+			canvas.TextWrap(left, y2-ys, ts, 40, s, green)
+			canvas.TextWrap(left, y3-ys, ts, 20, s, blue)
 			e.Frame(canvas.Context.Ops)
 		}
 	}
