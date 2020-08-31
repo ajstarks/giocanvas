f9a05979ddb39f7bfb005644b54476b9b943ed24
Anthony Starks <ajstarks@gmail.com>
2020-08-31 16:19:38 -0400

    make arcs go counter-clockwise

--- abs.go
+++ abs.go
@@ -244,7 +244,7 @@ func (c *Canvas) AbsEllipse(x, y, w, h float32, fillcolor color.RGBA) {
 }
 
 // AbsArc makes circulr arc centered at (x, y), through angles start and end;
-// the angles are measured in radians and increase clockwise
+// the angles are measured in radians and increase counter-clockwise.
 // N.B: derived from the clipLoader function in widget/material/loader.go
 func (c *Canvas) AbsArc(x, y, radius float32, start, end float64, fillcolor color.RGBA) {
 	ops := c.Context.Ops
@@ -269,7 +269,7 @@ func (c *Canvas) AbsArc(x, y, radius float32, start, end float64, fillcolor colo
 			angle = end
 		}
 		sins, coss := sine, cose
-		sine, cose = math.Sincos(angle)
+		sine, cose = math.Sincos(-angle)
 
 		// https://pomax.github.io/bezierinfo/#circles
 		div := 1.0 / (coss*sine - cose*sins)
