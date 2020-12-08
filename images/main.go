c1bc14083062b6e915060cb12b9ee926ec9061fb
Anthony Starks <ajstarks@gmail.com>
2020-12-08 16:25:04 -0500

    touch up images example

--- images/main.go
+++ images/main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"flag"
+	"image"
 	"image/color"
 	"os"
 
@@ -13,20 +14,39 @@ import (
 	"github.com/ajstarks/giocanvas"
 )
 
+func getimage(s string) (image.Image, error) {
+	i, err := os.Open(s)
+	if err != nil {
+		return nil, err
+	}
+	im, _, err := image.Decode(i)
+	if err != nil {
+		return nil, err
+	}
+	i.Close()
+	return im, nil
+}
+
 func images(title string, width, height float32) {
 	defer os.Exit(0)
 	win := app.NewWindow(app.Title(title), app.Size(unit.Px(width), unit.Px(height)))
 	canvas := giocanvas.NewCanvas(width, height, system.FrameEvent{})
+	im, err := getimage("earth.jpg")
+	if err != nil {
+		return
+	}
+	bgcolor := color.NRGBA{0, 0, 0, 255}
 	for e := range win.Events() {
 		switch e := e.(type) {
 		case system.FrameEvent:
 			var x, y, scale float32
-			scale = 5.0
+			scale = 2.0
+			canvas.Background(bgcolor)
 			canvas.Grid(0, 0, 100, 100, 0.1, 10, color.NRGBA{128, 128, 128, 255})
-			for x = 10; x < 100; x += 10 {
+			for x = 20; x <= 80; x += 10 {
 				y = x
-				canvas.CenterImage("earth.jpg", x, y, 1000, 1000, scale)
-				scale += 2.5
+				canvas.Img(im, x, y, 1000, 1000, scale)
+				scale += 2.0
 			}
 			e.Frame(canvas.Context.Ops)
 		case key.Event:
