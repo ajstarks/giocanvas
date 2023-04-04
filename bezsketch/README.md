08d3d491dba558f3076a86549abf78d680d11dc1
Anthony Starks <ajstarks@gmail.com>
2023-04-04 12:10:06 -0400

    update bezsketch to use giocanvas

---
+++ bezsketch/README.md
@@ -0,0 +1,15 @@
+# bezsketch
+
+![bezsketch](bezsketch.png)
+
+Sketch quadratic bezier curves
+
+* Button 1: define begin point
+* Button 2: define ending point
+* Drag: define control point
+
+command flags:
+
+	-width n : canvas width
+	-height n : canvas height
+
--- bezsketch/main.go
+++ bezsketch/main.go
@@ -9,14 +9,11 @@ import (
 	"os"
 
 	"gioui.org/app"
-	"gioui.org/f32"
 	"gioui.org/io/event"
 	"gioui.org/io/pointer"
 	"gioui.org/io/system"
-	"gioui.org/op"
-	"gioui.org/op/clip"
-	"gioui.org/op/paint"
 	"gioui.org/unit"
+	"github.com/ajstarks/giocanvas"
 )
 
 func main() {
@@ -39,56 +36,31 @@ func main() {
 	app.Main()
 }
 
-func curve(ops *op.Ops, x, y, cx, cy, ex, ey, size float32, strokecolor color.NRGBA) {
-	path := new(clip.Path)
-	// control and endpoints are relative to the starting point
-	ctrl := f32.Point{X: cx - x, Y: cy - y}
-	to := f32.Point{X: ex - x, Y: ey - y}
-
-	path.Begin(ops)
-	path.Move(f32.Point{X: x, Y: y})
-	path.Quad(ctrl, to)
-	stack := clip.Stroke{Path: path.End(), Width: size}.Op().Push(ops)
-	paint.Fill(ops, strokecolor)
-	stack.Pop()
-
-}
-
-func circle(ops *op.Ops, x, y, radius float32, fillcolor color.NRGBA) {
-	const k = 0.551915024494 // http://spencermortensen.com/articles/bezier-circle/
-	path := new(clip.Path)
-	path.Begin(ops)
-	path.Move(f32.Point{X: x + radius, Y: y})
-	path.Cube(f32.Point{X: 0, Y: radius * k}, f32.Point{X: -radius + radius*k, Y: radius}, f32.Point{X: -radius, Y: radius})    // SE
-	path.Cube(f32.Point{X: -radius * k, Y: 0}, f32.Point{X: -radius, Y: -radius + radius*k}, f32.Point{X: -radius, Y: -radius}) // SW
-	path.Cube(f32.Point{X: 0, Y: -radius * k}, f32.Point{X: radius - radius*k, Y: -radius}, f32.Point{X: radius, Y: -radius})   // NW
-	path.Cube(f32.Point{X: radius * k, Y: 0}, f32.Point{X: radius, Y: radius - radius*k}, f32.Point{X: radius, Y: radius})      // NE
-	path.Close()
-	stack := clip.Outline{Path: path.End()}.Op().Push(ops)
-	paint.ColorOp{Color: fillcolor}.Add(ops)
-	paint.PaintOp{}.Add(ops)
-	stack.Pop()
-}
-
 var pressed bool
 var mouseX, mouseY float32
-var bx, by, cx, cy, ex, ey float32
+var bx, by, ex, ey float32
 
-func mousePos(q event.Queue) {
+func pctcoord(x, y, width, height float32) (float32, float32) {
+	return 100 * (x / width), 100 - (100 * (y / height))
+}
+
+func textcoord(canvas *giocanvas.Canvas, x, y, size float32, color color.NRGBA) {
+	canvas.Circle(x, y, size/2, color)
+	canvas.TextMid(x, y+size, size, fmt.Sprintf("(%.1f, %.1f)", x, y), giocanvas.ColorLookup("black"))
+}
+
+func pctmousePos(q event.Queue, width, height float32) {
 	for _, ev := range q.Events(pressed) {
 		if p, ok := ev.(pointer.Event); ok {
 			switch p.Type {
 			case pointer.Drag:
-				mouseX = p.Position.X
-				mouseY = p.Position.Y
+				mouseX, mouseY = pctcoord(p.Position.X, p.Position.Y, width, height)
 			case pointer.Press:
 				switch p.Buttons {
 				case pointer.ButtonPrimary:
-					bx = p.Position.X
-					by = p.Position.Y
+					bx, by = pctcoord(p.Position.X, p.Position.Y, width, height)
 				case pointer.ButtonSecondary:
-					ex = p.Position.X
-					ey = p.Position.Y
+					ex, ey = pctcoord(p.Position.X, p.Position.Y, width, height)
 				}
 				pressed = true
 			}
@@ -97,28 +69,28 @@ func mousePos(q event.Queue) {
 }
 
 func bezsketch(w *app.Window, width, height float32) error {
-	beginColor := color.NRGBA{R: 0, G: 0, B: 255, A: 255}
-	endColor := color.NRGBA{R: 0, G: 255, B: 0, A: 255}
-	bx, by = 250.0, 500.0
-	cx, cy = 100.0, 100.0
-	ex, ey = 750.0, 500.0
+	beginColor := color.NRGBA{R: 0, G: 255, B: 0, A: 255}
+	endColor := color.NRGBA{R: 255, G: 0, B: 0, A: 255}
 	curveColor := color.NRGBA{R: 128, G: 128, B: 128, A: 128}
-	var ops op.Ops
+
+	bx, by = 25.0, 50.0
+	ex, ey = 75.0, 50.0
+	var cx, cy float32 = 10, 10
 	for {
 		ev := <-w.Events()
 		switch e := ev.(type) {
 		case system.DestroyEvent:
 			return e.Err
 		case system.FrameEvent:
-			ops.Reset()
-			pointer.InputOp{Tag: pressed, Grab: true, Types: pointer.Press | pointer.Drag}.Add(&ops)
-			mousePos(e.Queue)
+			canvas := giocanvas.NewCanvas(width, height, system.FrameEvent{})
+			pointer.InputOp{Tag: pressed, Grab: false, Types: pointer.Press | pointer.Drag}.Add(canvas.Context.Ops)
+			textcoord(canvas, bx, by, 2, beginColor)
+			textcoord(canvas, ex, ey, 2, endColor)
+			canvas.QuadStrokedCurve(bx, by, cx, cy, ex, ey, 1, curveColor)
+			fmt.Printf("curve %.1f %.1f %.1f %.1f %.1f %.1f\n", bx, by, cx, cy, ex, ey)
+			pctmousePos(e.Queue, width, height)
 			cx, cy = mouseX, mouseY
-			fmt.Printf("curve %v %v %v %v %v %v\n", bx, by, cx, cy, ex, ey)
-			circle(&ops, bx, by, 10, beginColor)
-			circle(&ops, ex, ey, 10, endColor)
-			curve(&ops, bx, by, cx, cy, ex, ey, 10, curveColor)
-			e.Frame(&ops)
+			e.Frame(canvas.Context.Ops)
 		}
 	}
 }
