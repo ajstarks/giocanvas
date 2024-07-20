1f4ff320db860c88582b1cac938b07e102d130fa
Anthony Starks <ajstarks@gmail.com>
2024-07-20 18:32:15 -0400

    fix title in polar

--- polar/main.go
+++ polar/main.go
@@ -14,8 +14,9 @@ import (
 
 func polar(title string, width, height float32) {
 	w := &app.Window{}
-	w.Option(app.Title("polar"), app.Size(unit.Dp(width), unit.Dp(height)))
-
+	w.Option(app.Title(title), app.Size(unit.Dp(width), unit.Dp(height)))
+	topcolor := color.NRGBA{255, 0, 0, 100}
+	botcolor := color.NRGBA{0, 0, 255, 100}
 	for {
 		e := w.Event()
 		switch e := e.(type) {
@@ -28,11 +29,11 @@ func polar(title string, width, height float32) {
 			for radius = 2; radius < 50; radius += 2 {
 				for theta = 180; theta <= 360; theta += 15 {
 					x, y := canvas.PolarDegrees(50, 50, radius, theta)
-					canvas.Circle(x, y, radius/12, color.NRGBA{255, 0, 0, 100})
+					canvas.Circle(x, y, radius/12, topcolor)
 				}
 				for theta = math.Pi / 16; theta < math.Pi; theta += math.Pi / 16 {
 					x, y := canvas.Polar(50, 50, radius, theta)
-					canvas.Circle(x, y, radius/12, color.NRGBA{0, 0, 255, 100})
+					canvas.Circle(x, y, radius/12, botcolor)
 				}
 			}
 			e.Frame(canvas.Context.Ops)
@@ -45,6 +46,6 @@ func main() {
 	cw := flag.Int("width", 1000, "canvas width")
 	ch := flag.Int("height", 1000, "canvas height")
 	flag.Parse()
-	go polar("polar", float32(*cw), float32(*ch))
+	go polar("polar coordinates", float32(*cw), float32(*ch))
 	app.Main()
 }
