399db7610dded20fde61f5885ba139d7a7433fef
Anthony Starks <ajstarks@gmail.com>
2024-07-29 11:35:47 -0400

    hello: simplify

--- hello/main.go
+++ hello/main.go
@@ -4,7 +4,6 @@ package main
 import (
 	"flag"
 	"image/color"
-	"io"
 	"os"
 
 	"gioui.org/app"
@@ -17,36 +16,22 @@ func main() {
 	flag.IntVar(&cw, "width", 1000, "canvas width")
 	flag.IntVar(&ch, "height", 1000, "canvas height")
 	flag.Parse()
-
-	width := float32(cw)
-	height := float32(ch)
-
-	go func() {
-		w := &app.Window{}
-		w.Option(app.Title("hello"), app.Size(unit.Dp(width), unit.Dp(height)))
-		if err := hello(w); err != nil {
-			io.WriteString(os.Stderr, "Cannot create the window\n")
-			os.Exit(1)
-		}
-		os.Exit(0)
-	}()
+	go hello(cw, ch)
 	app.Main()
 }
 
-func hello(w *app.Window) error {
-	black := color.NRGBA{0, 0, 0, 255}
-	blue := color.NRGBA{0, 0, 255, 255}
-	white := color.NRGBA{255, 255, 255, 255}
+func hello(width, height int) {
+	w := &app.Window{}
+	w.Option(app.Title("hello"), app.Size(unit.Dp(float32(width)), unit.Dp(float32(height))))
 	for {
 		switch e := w.Event().(type) {
 		case app.DestroyEvent:
-			return e.Err
+			os.Exit(0)
 		case app.FrameEvent:
 			canvas := giocanvas.NewCanvas(float32(e.Size.X), float32(e.Size.Y), app.FrameEvent{})
-			canvas.CenterRect(50, 50, 100, 100, black)
-			canvas.Circle(50, 0, 50, blue)
-			canvas.TextMid(50, 20, 10, "hello, world", white)
-			canvas.CenterImage("earth.jpg", 50, 70, 1000, 1000, 30)
+			canvas.Background(color.NRGBA{0, 0, 0, 255})
+			canvas.Image("earth.jpg", 100, 0, 1000, 1000, 100)
+			canvas.Text(10, 70, 10, "hello, world", color.NRGBA{255, 255, 255, 255})
 			e.Frame(canvas.Context.Ops)
 		}
 	}
