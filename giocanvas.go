cdcc6c57f80f76b4185989326dd9eb4261909a82
Anthony Starks <ajstarks@gmail.com>
2024-07-29 20:31:25 -0400

    refactor NewCanvas... to use common code

--- giocanvas.go
+++ giocanvas.go
@@ -21,14 +21,14 @@ type Canvas struct {
 	Context       layout.Context
 }
 
-// NewCanvas initializes a Canvas
-func NewCanvas(width, height float32, e app.FrameEvent) *Canvas {
+// setupCanvas sets up common canvas items
+func setupCanvas(width, height float32, e app.FrameEvent, f []font.FontFace) *Canvas {
 	canvas := new(Canvas)
 	canvas.Width = width
 	canvas.Height = height
 	canvas.TextColor = color.NRGBA{0, 0, 0, 255}
 	theme := material.NewTheme()
-	theme.Shaper = text.NewShaper(text.NoSystemFonts(), text.WithCollection(gofont.Regular()))
+	theme.Shaper = text.NewShaper(text.NoSystemFonts(), text.WithCollection(f))
 	canvas.Theme = theme
 	canvas.Context = app.NewContext(new(op.Ops), e)
 	iw, ih := int(width), int(height)
@@ -39,19 +39,12 @@ func NewCanvas(width, height float32, e app.FrameEvent) *Canvas {
 	return canvas
 }
 
+// NewCanvas initializes a Canvas using the default font set
+func NewCanvas(width, height float32, e app.FrameEvent) *Canvas {
+	return setupCanvas(width, height, e, gofont.Regular())
+}
+
+// NewCanvasFonts initializes a canvas with a specified set of set of fonts
 func NewCanvasFonts(width, height float32, fonts []font.FontFace, e app.FrameEvent) *Canvas {
-	canvas := new(Canvas)
-	canvas.Width = width
-	canvas.Height = height
-	canvas.TextColor = color.NRGBA{0, 0, 0, 255}
-	theme := material.NewTheme()
-	theme.Shaper = text.NewShaper(text.NoSystemFonts(), text.WithCollection(fonts))
-	canvas.Theme = theme
-	canvas.Context = app.NewContext(new(op.Ops), e)
-	iw, ih := int(width), int(height)
-	canvas.Context.Constraints.Min.X = iw
-	canvas.Context.Constraints.Min.Y = ih
-	canvas.Context.Constraints.Max.X = iw
-	canvas.Context.Constraints.Max.Y = ih
-	return canvas
+	return setupCanvas(width, height, e, fonts)
 }
