50ca7bf3a4f31ac691ded9c1fa1c3dc4e5251bc4
Anthony Starks <ajstarks@gmail.com>
2024-09-04 22:14:38 -0400

    compchart: update to new label API

--- compchart/main.go
+++ compchart/main.go
@@ -32,7 +32,7 @@ func comp(canvas *giocanvas.Canvas) error {
 	cosine.Zerobased = false
 	sine.Zerobased = false
 	cosine.Frame(canvas, 5)
-	sine.Label(canvas, 1.5, 10)
+	sine.Label(canvas, 1.5, 10, "", "gray")
 	cosine.YAxis(canvas, 1.2, -1.0, 1.0, 0.5, "%0.2f", true)
 	cosine.Color = color.NRGBA{0, 128, 0, 255}
 	sine.Color = color.NRGBA{128, 0, 0, 255}
