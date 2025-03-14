d158cb9621709644b2601ed16a4e12b541361513
Anthony Starks <ajstarks@gmail.com>
2025-03-13 23:53:52 -0400

    gchart: update browser data

--- gchart/browser.d
+++ gchart/browser.d
@@ -1,6 +1,6 @@
-# Browser Market Share June 2020
-Chrome	65.47	rgb(211,57,53)
-Safari	16.97	rgb(13,107,202)
-Other	11.25	rgb(150,150,150)
-Firefox	4.25	rgb(228,158,21)
-IE/Edge	2.06	rgb(0,128,0)
+# Browser Market Share February 2025
+Chrome	66.63	rgb(211,57,53)
+Safari	17.73	rgb(13,107,202)
+Other	7.58	rgb(150,150,150)
+Edge	5.39	rgb(0,128,0)
+Firefox	2.67	rgb(228,158,21)
