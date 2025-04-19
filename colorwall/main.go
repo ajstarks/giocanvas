2f7b59538b536a02a32da5f2804a6e7598faed7e
Anthony Starks <ajstarks@gmail.com>
2025-04-19 15:25:36 -0400

    colorwall: simplify

--- colorwall/main.go
+++ colorwall/main.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"image/color"
 	"os"
 
 	"gioui.org/app"
@@ -9,26 +10,9 @@ import (
 	"github.com/ajstarks/giocanvas"
 )
 
-var palette = []string{
-	"#000000",
-	"#8a1f1b", "#a74e4a", "#af5d23", "#ccb04d", "#03342f",
-	"#122e63", "#391a32", "#735976", "#5e825e", "#366e93",
-	"#eeeeee",
-}
-
-func displaypalette(canvas *giocanvas.Canvas, x, y, xincr, yincr float32) {
-	for i := 0; i < len(palette); i++ {
-		canvas.Square(x, y, yincr, giocanvas.ColorLookup(palette[i]))
-		canvas.CText(x, y-yincr, 1.5, palette[i], giocanvas.ColorLookup("#000000"))
-		x += xincr
-	}
-}
-
-func colorwall(width, height float32, showpalette bool) {
-	w := new(app.Window)
-	appsize := app.Size(unit.Dp(width), unit.Dp(height))
-	w.Option(app.Title(`colorwall: inspired by Ellsworth Kelly's "Colors on a Large Wall", 1951`), appsize)
-	layout := [][]string{
+var (
+	bgcolor = color.NRGBA{0xbb, 0xbb, 0xbb, 0xff}
+	layout  = [][]string{
 		{"#000000", "#eeeeee", "#735976", "#eeeeee", "#000000", "#af5d23", "#eeeeee", "#366e93"}, // row 1
 		{"#eeeeee", "#03342f", "#000000", "#eeeeee", "#ccb04d", "#eeeeee", "#a74e4a", "#000000"}, // row 2
 		{"#000000", "#eeeeee", "#eeeeee", "#391a32", "#eeeeee", "#eeeeee", "#eeeeee", "#af5d23"}, // row 3
@@ -38,32 +22,33 @@ func colorwall(width, height float32, showpalette bool) {
 		{"#eeeeee", "#a74e4a", "#5e825e", "#eeeeee", "#000000", "#735976", "#eeeeee", "#eeeeee"}, // row 7
 		{"#000000", "#eeeeee", "#391a32", "#ccb04d", "#eeeeee", "#000000", "#a74e4a", "#000000"}, // row 8
 	}
-	var x, y, left, right, top, bottom, xincr, yincr, nc, nr float32
-	left, right, bottom, top = 25, 85, 20, 80
-	nr, nc = 8, 8
+)
 
-	xincr = (right - left) / nr
-	yincr = (top - bottom) / nc
-	bgcolor := giocanvas.ColorLookup("#dddddd")
-	basecolor := giocanvas.ColorLookup("#bbbbbb")
+func colorwall(width, height float32) {
+	w := new(app.Window)
+	appsize := app.Size(unit.Dp(width), unit.Dp(height))
+	w.Option(app.Title("colorwall: inspired by Ellsworth Kelly's “Colors on a Large Wall”, 1951"), appsize)
+
+	var x, y, left, right, top, bottom, xincr, yincr float32
+	left, right, bottom, top = 25, 85, 20, 80
+	nr, nc := 8, 8
+
+	xincr = (right - left) / float32(nr)
+	yincr = (top - bottom) / float32(nc)
+
 	for {
 		switch e := w.Event().(type) {
 		case app.FrameEvent:
 			canvas := giocanvas.NewCanvas(width, height, app.FrameEvent{})
 			canvas.Background(bgcolor)
-			canvas.Square(51.25, 53.75, 60.5, basecolor)
 			y = top
-			for i := 0; i < len(layout); i++ {
-				row := layout[i]
+			for i := 0; i < nr; i++ {
 				x = left
-				for j := 0; j < len(row); j++ {
-					canvas.Square(x, y, yincr-0.1, giocanvas.ColorLookup(row[j]))
+				for j := 0; j < nc; j++ {
+					canvas.Square(x, y, yincr-0.1, giocanvas.ColorLookup(layout[i][j]))
 					x += xincr
 				}
 				y -= yincr
-			}
-			if showpalette {
-				displaypalette(canvas, 10, 10, xincr, yincr)
 			}
 			e.Frame(canvas.Context.Ops)
 
@@ -74,6 +59,6 @@ func colorwall(width, height float32, showpalette bool) {
 }
 
 func main() {
-	go colorwall(1000, 1000, false)
+	go colorwall(1000, 1000)
 	app.Main()
 }
